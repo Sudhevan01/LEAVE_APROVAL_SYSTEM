7769aa1decc0ce29154bac879cdf7a7d00c5d4f4
Sudhevan Y <zodiacsudhevan@gmail.com>
2026-05-13 00:25:07 +0530

    Added database models and admin panel

--- leaveapp/admin.py
+++ leaveapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# pyrefly: ignore [missing-import]
+from .models import Employee,LeaveRequest
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(LeaveRequest)
--- leaveapp/models.py
+++ leaveapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=100)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class LeaveRequest(models.Model):
+
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Rejected', 'Rejected'),
+    ]
+
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+    leave_type = models.CharField(max_length=50)
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    reason = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='Pending'
+    )
+
+    applied_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.employee.name} - {self.status}"

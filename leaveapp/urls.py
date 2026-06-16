2450bb948152de53f81dda48a2813c2187f4386f
Sudhevan Y <zodiacsudhevan@gmail.com>
2026-06-16 18:37:45 +0530

    created registerpage, login page

--- leaveapp/urls.py
+++ leaveapp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from django.shortcuts import redirect
 # pyrefly: ignore [missing-import]
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', lambda request: redirect('login')),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('approve/<int:leave_id>/', views.approve_leave, name='approve_leave'),
     path('reject/<int:leave_id>/', views.reject_leave, name='reject_leave'),
-]
+]
--- leaveapp/views.py
+++ leaveapp/views.py
@@ -1,11 +1,41 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 # pyrefly: ignore [missing-import]
 from .models import Employee,LeaveRequest
 
 def login_view(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+
+            login(request, user)
+            if user.isstaff:
+                return redirect('admin-dashboard')
+            else:
+                return redirect('dashboard')
     return render(request, 'login.html')
 
 
+def logout_view(request):
+
+    logout(request)
+
+    return redirect('login')
+
+@login_required
 def dashboard(request):
 
     employee = Employee.objects.first()
@@ -39,15 +69,25 @@ def dashboard(request):
 
     return render(request, 'dashboard.html', context)
 
+@login_required
 def admin_dashboard(request):
+
+    if not request.user.is_staff:
+        return HttpResponse("Unauthorized Access")
 
     leaves = LeaveRequest.objects.all()
 
     context = {
-        'leaves': leaves
+        'leaves': leaves,
+        'total': leaves.count(),
+        'pending': leaves.filter(status='Pending').count(),
+        'approved': leaves.filter(status='Approved').count(),
+        'rejected': leaves.filter(status='Rejected').count(),
     }
 
-    return render(request, 'admin_dashboard.html', context)
+    return render(request,
+                  'admin_dashboard.html',
+                  context)
 
 
 def approve_leave(request, leave_id):
@@ -106,6 +146,22 @@ def admin_dashboard(request):
         'rejected': leaves.filter(status='Rejected').count(),
     }
 
-    return render(request,
-                  'admin_dashboard.html',
-                  context)
+    return render(request,'admin_dashboard.html',context)
+
+def register_view(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        return redirect('login')
+
+    return render(request, 'register.html')
